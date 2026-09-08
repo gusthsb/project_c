c33a6ba3bae51a308e6026dede2fbd2678be117e
gusthsb <gustavo.hdsbrito@gmail.com>
2026-09-08 18:01:22 -0300

    fix: separado por modulo

--- dice_roll.py
+++ dice_roll.py
@@ -1,30 +1,7 @@
 #!/usr/bin/env python3
 
-from abc import ABC, abstractmethod
-import typing
+from casino_base import CasinoGame
 import random
-
-
-class CasinoGame(ABC):
-    def __init__(self, name: str) -> None:
-        super().__init__()
-        self.name = name
-
-    @abstractmethod
-    def without_choice_play(self, bet_amount: float) -> float:
-        """
-        Recebe o valor da aposta e retorna o prêmio.
-        Se perder, retorna 0.0
-        """
-        pass
-
-    @abstractmethod
-    def with_choice_play(self, bet_amount: float, bet_choice: str) -> float:
-        """
-        Recebe o valor da aposta e retorna o prêmio.
-        Se perder, retorna 0.0
-        """
-        pass
 
 
 class DiceGame(CasinoGame):
