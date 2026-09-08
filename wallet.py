4b84d55736bb349c9de9644972adab2bf1cac89a
gusthsb <gustavo.hdsbrito@gmail.com>
2026-09-08 18:09:32 -0300

    feat: create class PlayerWallet

---
+++ wallet.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+class PlayerWallet:
+    def __init__(self, initial_balance: float = 100.0) -> None:
+        self.balance: float = initial_balance
+
+    def try_place_bet(self, amount: float) -> bool:
+        """Tenta fazer uma aposta. Retorna True se tiver saldo"""
+        if amount > self.balance:
+            print(f"Saldo insuficiente! Você tem no momento R${self.balance:.2f}.")
+            return False
+        else:
+            self.balance -= amount
+            return True
+
+    def add_winnings(self, amount: float) -> None:
+        """Adiciona na conta o dinheiro recebido pelas apostas"""
+        self.balance += amount
+
+    
